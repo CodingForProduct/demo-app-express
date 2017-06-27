882957492ce63cda6e9142509a18f3f0eec025b3
Wai-Yin Kwan <wykhuh@icloud.com>
2017-06-27 03:02:01 -0700

    display a list of teams

--- app.js
+++ app.js
@@ -17,12 +17,18 @@ app.get('/', function (request, response) {
 
 // array of users
 var workshopUsers = [
-  { id: 1, name: "Amy Jiang" },
-  { id: 2, name: "Bridget Dickens" },
-  { id: 3, name: "Carolina Bravo" },
-  { id: 4, name: "Christa Timil Keyes-Venson" },
-  { id: 5, name: "Cindy Wang" }
+  { id: 1, name: "Amy Jiang", team_id: 1 },
+  { id: 2, name: "Bridget Dickens", team_id: 2 },
+  { id: 3, name: "Carolina Bravo", team_id: 2 },
+  { id: 4, name: "Christa Timil Keyes-Venson", team_id: 1 },
+  { id: 5, name: "Cindy Wang" , team_id: 2}
 ];
+
+// array of teams
+var workshopTeams = [
+  { id: 1, name: "Team One" },
+  { id: 2, name: "Team Two" },
+]
 
 // display list of users
 app.get('/users', function (request, response) {
@@ -44,6 +50,12 @@ app.get('/users/:id', function (request, response) {
   // pass data to template
   response.render('user', { user: targetUser[0] });
 });
+
+app.get('/teams', function (request, response) {
+  // pass data to template
+  response.render('teams', { teams: workshopTeams });
+});
+
 
 // start server on port
 app.listen(3000, function() {
