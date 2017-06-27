6fa9c56cef218f61a8a3c541e0bd6ae0082233cf
Wai-Yin Kwan <wykhuh@icloud.com>
2017-06-27 02:18:46 -0700

    3. add home page template

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 // libraries that this app needs
 var express = require('express');
+var path = require('path');
 
 // initialize app
 var app = express();
 
+// set the type of template that the app will use
+app.set('view engine', 'ejs');
+// set the directory for the templates
+app.set('views', path.join(__dirname, 'views'));
+
 // display root route
 app.get('/', function (request, response) {
-  response.send('Hi.');
+  response.render('home');
 });
 
 // start server on port
