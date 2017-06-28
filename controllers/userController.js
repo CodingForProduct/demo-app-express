a17f6b67f3e268dcf80f58c415480fe3421a1349
Wai-Yin Kwan <wykhuh@icloud.com>
2017-06-28 05:46:20 -0700

    retrieve one  user from database

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,12 +12,18 @@ exports.getUsers = function (request, response, next) {
   });
 }
 
-exports.getUser = function (request, response) {
+exports.getUser = function (request, response, next) {
   // get the id listed in the url
-  var id = request.params.id;
-  // find the user with a given idea
-  var targetUser = User.findOne(Number(id));
+  var id = Number(request.params.id);
 
-  // pass data to template
-  response.render('user', { user: targetUser });
+  // find the user with a given id
+  User.findOne(id)
+  .then(function(res) {
+    // pass data to template
+    response.render('user', { user: res || {} });
+  })
+  .catch(function(err) {
+    console.log('getUser err:', err)
+    next();
+  });
 }
--- models/User.js
+++ models/User.js
@@ -1,18 +1,9 @@
-// array of users
-var workshopUsers = [
-  { id: 1, name: "Amy Jiang", team_id: 1 },
-  { id: 2, name: "Bridget Dickens", team_id: 2 },
-  { id: 3, name: "Carolina Bravo", team_id: 2 },
-  { id: 4, name: "Christa Timil Keyes-Venson", team_id: 1 },
-  { id: 5, name: "Cindy Wang" , team_id: 2}
-];
+var db = require('../config/database');
 
 exports.findAll = function () {
   return db.select().from('users').orderBy('name');
 }
 
 exports.findOne = function (id) {
-  return workshopUsers.filter(function(user){
-    return user.id === id;
-  })[0];
+  return db.first().from('users').where('id', id);
 }
