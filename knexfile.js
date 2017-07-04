e3519945de3a1a1d86b3b9f94bdf590fa402cbd8
Wai-Yin Kwan <wykhuh@icloud.com>
2017-07-04 07:39:49 -0700

    19. connect to database

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+require('./config/environment');
+
+var pgDefaults = {
+  client: 'postgresql',
+  connection: process.env.DATABASE_URL,
+  pool: {
+    min: 0,
+    max: 2,
+  },
+  migrations: {
+    directory: __dirname + '/migrations',
+    tableName: 'migrations'
+  },
+};
+
+module.exports = {
+  development: pgDefaults,
+  staging: pgDefaults,
+  production: pgDefaults
+};
